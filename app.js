1d089671b2b4855f2de9f2d7d853004ac3a83a1e
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-07-08 17:58:43 +0300

    lesson2

--- app.js
+++ app.js
@@ -1,14 +1,124 @@
-//mkdir services - (в консолі) - створить папку services
-//.. - (в консолі) - папку назад
-//dir - (в консолі) - виводить всі файли папки
-//rmdir services - (в консолі) - видаляє папку services
-// cd- change directory
-//KISS- keep it simple(пекти  хліб)
-//YAGNI- You aren`t gonna need it (то шо не треба стираємо)
-//SOLID- розділяти на модулі, сервіси, підфайли
+// методи response: res.end, res.write, res.json, res.status(404).json('NOT FOUND')
+// http методи: get(read), post(create), put(update/replace), delete(delete), patch(update/modify)
+// req- то шо нам послали(наприклад з постмена)
+//res.end() -можна пустим залишити(щоб закінчити response), віддає переважно лише стрічку з повідомленням, json віддавати не вміє
+//(req.params) зберігаються всі параметри з урли
 
-const {createUser} = require('./services/user.service')
-require('./services/file.service')
+// app.get('/', (req, res) => {
+  //res.write('hello') -можна пустим залишити(щоб закінчити response), віддає переважно лише стрічку з повідомленням, json віддавати не вміє
+  //res.write('hello') -можна пустим залишити(щоб закінчити response), віддає переважно лише стрічку з повідомленням, json віддавати не вміє
+  //res.write('hello') -можна пустим залишити(щоб закінчити response), віддає переважно лише стрічку з повідомленням, json віддавати не вміє
+  //res.end() -всеодно треба, щоб перестало прогружатись(чекати завершення відповіді)
+// })\
 
-const user1 = createUser('Emily', 23);
-console.log(user1);
+// endpoint це app.get('/users/:userId/delete' (кін. урла)
+
+//функція обробник(controller): (req, res) => {
+//   users.push({
+//     name: 'Test',
+//     age: Math.floor(Math.random()*100)
+//   })
+//
+//   res.status(201).json('User was created')
+// }
+
+const express = require('express')
+const users = require('./dataBase/users');
+const app = express()
+
+// app.get('/', (req, res) => {
+//   res.json('HELLO EXPRESS!')
+// })
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+
+app.get('/users/create', (req, res) => {
+
+  users.push({
+    name: 'Test',
+    age: Math.floor(Math.random()*100)
+  })
+
+  res.status(201).json('User was created')
+})
+// app.post('/user', (req, res) => {
+//
+// })
+
+app.get('/users/:userId/update', (req, res) => {
+
+  users.push({
+    name: 'Test',
+    age: Math.floor(Math.random()*100)
+  })
+
+  res.status(201).json('User was created')
+})
+// app.put('/users/:userId', (req, res) => {
+//
+// })
+
+app.get('/users/:userId/delete', (req, res) => {
+
+  users.push({
+    name: 'Test',
+    age: Math.floor(Math.random()*100)
+  })
+
+  res.status(201).json('User was created')
+})
+// app.delete('/users/:userId', (req, res) => {
+//
+// })
+
+app.get('/users/:userId', (req, res) => {
+  console.log('Customer want to get user with ID 1')
+
+  const userIndex= +req.params.userId;
+
+  if(isNaN(userIndex) || userIndex<0){
+    //400- bad request
+    res.status(400).json('Please enter valid Id')
+    //завершуємо виконання нашої ф-ї (тільки return)
+    return;
+  }
+
+  const user = users[userIndex]
+
+  if(!user){
+    res.status(404).json(`User with Id ${userIndex} is not found`)
+    return;
+  }
+
+  res.json(users[userIndex])
+})
+
+// дз:
+// Create ствоити
+// Read зчитати :getAll(/users), getOne(/users/:userId)
+// Update
+// Delete
+const fs = require("fs/promises");
+app.get('/users',async (req,res)=>{
+  let buffer = await fs.readFile('./ba');
+  res.json(buffer.toString())
+})
+
+const axios = require("axios");
+app.get('/', async (req,res)=>{
+ const resp = await axios.get('https://jsonplaceholder.typicode.com/users');
+ res.status(resp.status).json(resp.data)
+
+})
+
+
+
+
+
+
+app.listen(5000, ()=>{
+  console.log(  `Server listen port 5000`)
+})
