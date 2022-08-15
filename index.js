cb8a25702b3f30c2e8f3e68b0198fe8047ad4fb3
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-08-15 19:11:29 +0300

    dev update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 app.use(expressFileUpload());
 
-app.use('/ping', (req, res)=>res.json('pong'))
+app.use('/ping', (req, res)=>res.json('PONG'))
 app.use('/auth', authRouter)
 app.use('/users', userRouter)
 
