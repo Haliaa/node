e67082bbcbd9cd1118440a9e728d71641ecfe5c3
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-08-25 11:59:48 +0300

    sockets

--- app.js
+++ app.js
@@ -14,10 +14,12 @@
 require('dotenv').config()
 
 const express = require('express')
+const http = require('http')
 const expressFileUpload = require('express-fileupload')
 const swaggerUi = require('swagger-ui-express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const socketIo = require('socket.io');
 
 mongoose.connect('mongodb://localhost:27017/dec');
 
@@ -29,6 +31,60 @@ const swaggerJson = require("./swagger.json");
 
 const app = express();
 
+const server = http.createServer(app);
+const io = socketIo(server, {cors: 'http://localhost:63342'});
+io.on('connection', (socket) => {
+    console.log('_____________')
+    console.log(socket.handshake.query)
+    console.log(socket.handshake.auth)
+    console.log('_____________')
+
+    socket.emit('connectSuccess', {hello: 'World'})
+
+    socket.on('sendMessage', (messageData) => {
+        console.log('Socket ', socket.id, 'with auth token ', socket.handshake.auth.token, 'send message ', messageData)
+
+        //emit to all users except sender
+        socket.broadcast.emit('message:received', {
+            user: 'Chudik',
+            message: 'Hello world'
+        });
+
+        // socket.broadcast.emit('message:received2',{
+        //     user: 'Chudik',
+        //     message: 'Hello world'
+        // });
+        // socket.broadcast.emit('message:received3',{
+        //     user: 'Chudik',
+        //     message: 'Hello world'
+        // });
+
+        // //5 повідомлень з однакоовими текстовками
+        // for (let i = 0; i < 5; i++) {
+        //     const messageNumber = messageData[i];
+        //
+        //     socket.broadcast.emit('messageNumber', {
+        //         user: 'Chudik',
+        //         message: 'Hello world'
+        //     });
+        // }
+
+        setTimeout(() => {
+            io.emit('globalBroadcast', 'TEST SOCKET')
+        }, 2000)
+    });
+
+    socket.on('room:join', (joinInfo)=>{
+        socket.join(joinInfo.roomId);
+
+        // //to all room members
+        // io.to(joinInfo.roomId).emit('room:newMember', {id:socket.id});
+
+        //to all room members except sender
+        socket.to(joinInfo.roomId).emit('room:newMember', {id:socket.id})
+    })
+
+})
 
 
 app.use(express.json())
@@ -44,7 +100,7 @@ if (NODE_ENV !== 'prod') {
 app.use(expressFileUpload());
 
 //cors(_configureCors()) - покриває лиш один ендпоінт як middleware
-app.use('/auth', cors(_configureCors()),authRouter);
+app.use('/auth', cors(_configureCors()), authRouter);
 app.use('/users', userRouter);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJson))
 // app.use('/docs', swaggerUi.serve)
@@ -67,7 +123,7 @@ app.use('*', (req, res) => {
 })
 
 
-app.listen(5000, () => {
+server.listen(5000, () => {
     console.log(`Server listen port 5000`);
     cronRun()
 })
@@ -78,7 +134,7 @@ app.listen(5000, () => {
 // app.delete('/users/:id',userController.deleteUser)
 // app.put('/users/:id',userController.updateUser)
 
-function _configureCors () {
+function _configureCors() {
     const whitelist = CORS_WHITE_LIST.split(';')
 
     return {
